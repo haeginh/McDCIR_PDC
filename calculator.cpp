4a78881f3ea6088ebf9f503918cc1666ac2b0b75
haeginh <haeginh@gmail.com>
2021-12-15 14:42:34 +0900

    changed patient name

--- calculator.cpp
+++ calculator.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
 
 	// Set mandatory initialization classes
 	auto det = new DetectorConstruction();
-	det->SetPatientName("patient");
+	det->SetPatientName("Patient_M_H175M83800_fixed");
 	det->RegisterParallelWorld(new ParallelMesh("parallel"));
 	runManager->SetUserInitialization(det);
 	G4VModularPhysicsList* physicsList = new FTFP_BERT;
